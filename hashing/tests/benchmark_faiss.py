a72f5b468e10374b599d72c3e479cf7833af82a0
Matthieu Griffonnet <matthieu.griffonnet@etu.unice.fr>
2026-06-04 13:46:35 +0700

    feat(benchmark): extend FAISS benchmarking to include time measurement and additional k values

--- hashing/tests/benchmark_faiss.py
+++ hashing/tests/benchmark_faiss.py
@@ -4,13 +4,14 @@ Benchmarking Faiss against the brute-force method for nearest neighbor search.
 
 import os
 import sys
+from time import time
 
 sys.path.insert(0,
                 os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from retreiver.faiss_retriever import ImageRetriever
 
-K_VALUES = [1, 3, 5, 10]
+K_VALUES = [1, 3, 5, 10, 20, 50, 100]
 
 def benchmark_faiss(faiss_retriever: ImageRetriever, dataset_folder: str, k_values: list[int], csv_results: str = None):
     """
@@ -24,14 +25,18 @@ def benchmark_faiss(faiss_retriever: ImageRetriever, dataset_folder: str, k_valu
     print(f"Evaluating Faiss retriever ({faiss_retriever.model_size}) for k={k_values}...")
 
     for k in k_values:
+        start_time = time()
         average_distance, match_rate = faiss_retriever.evaluate(
             input_folder=dataset_folder, display_results=False, k=k
         )
         print(f"  k={k}: match_rate={match_rate:.2%}, avg_distance={average_distance:.4f}")
 
+        end_time = time()
+        time_taken = end_time - start_time
+
         if csv_results:
             with open(csv_results, "a") as f:
-                f.write(f"{faiss_retriever.model_size},{k},{average_distance:.4f},{match_rate:.2%}\n")
+                f.write(f"{faiss_retriever.model_size},{k},{average_distance:.4f},{match_rate:.2%},{time_taken:.2f}\n")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -43,7 +48,7 @@ if __name__ == "__main__":
 
     os.makedirs(os.path.dirname(CSV_RESULTS), exist_ok=True)
     with open(CSV_RESULTS, "w") as f:
-        f.write("model_size,k,average_distance,match_rate\n")
+        f.write("model_size,k,average_distance,match_rate,time\n")
 
     for model_size in ImageRetriever.MODEL_SIZES:
         faiss_retriever = ImageRetriever(model_size=model_size, index_dir=DATASET_FOLDER)
